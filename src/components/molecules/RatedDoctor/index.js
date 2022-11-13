0d6b0e733da1a11471f5508deb91caf0345603c1
Coderaulia <coderaulia@gmail.com>
2022-11-13 13:53:38 +0700

    create rated doctor, news item, and fixing styling

--- src/components/molecules/RatedDoctor/index.js
+++ src/components/molecules/RatedDoctor/index.js
@@ -1,12 +1,46 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
+import {fonts, colors} from '../../../utils';
+import {DummyDoctor1, IconStar} from '../../../assets';
 
 export default function RatedDoctor() {
   return (
-    <View>
-      <Text>Rated Doctor List</Text>
-    </View>
+    <TouchableOpacity style={styles.container}>
+      <Image source={DummyDoctor1} style={styles.avatar} />
+      <View style={styles.profile}>
+        <Text style={styles.name}>Alexa Rachel</Text>
+        <Text style={styles.category}>Dokter Umum</Text>
+      </View>
+      <View style={styles.rate}>
+        <IconStar />
+        <IconStar />
+        <IconStar />
+        <IconStar />
+        <IconStar />
+      </View>
+    </TouchableOpacity>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 16,
+    alignItems: 'center',
+  },
+  avatar: {width: 50, height: 50, borderRadius: 50 / 2, marginRight: 12},
+  rate: {flexDirection: 'row'},
+  profile: {flex: 1},
+  name: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+  },
+  category: {
+    fontSize: 12,
+    fontFamily: fonts.primary.normal,
+    color: colors.text.secondary,
+    marginTop: 2,
+  },
+});
--- src/pages/Doctors/index.js
+++ src/pages/Doctors/index.js
@@ -14,30 +14,41 @@ export default function Doctors() {
     <View style={styles.page}>
       {/* Main content for the border radius */}
       <View style={styles.content}>
-        <HomeProfile />
-        <Text style={styles.welcome}>
-          Mau konsultasi dengan siapa hari ini?
-        </Text>
-        {/* Create a scrollable view */}
-        <View style={styles.wrapperScroll}>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            <View style={styles.category}>
-              <Gap width={6} />
-              <DoctorCategory />
-              <DoctorCategory />
-              <DoctorCategory />
-              <DoctorCategory />
-            </View>
-          </ScrollView>
-        </View>
-        <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
-        <RatedDoctor />
-        <RatedDoctor />
-        <RatedDoctor />
-        <Text style={styles.sectionLabel}>Berita Terbaru</Text>
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.wrapperSection}>
+            {/* Adding gap on the top */}
+            <Gap height={30} />
+            <HomeProfile />
+            <Text style={styles.welcome}>
+              Mau konsultasi dengan siapa hari ini?
+            </Text>
+          </View>
+          {/* Create a scrollable view */}
+          <View style={styles.wrapperScroll}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <View style={styles.category}>
+                <Gap width={32} />
+                <DoctorCategory />
+                <DoctorCategory />
+                <DoctorCategory />
+                <DoctorCategory />
+                <Gap width={22} />
+              </View>
+            </ScrollView>
+          </View>
+          <View style={styles.wrapperSection}>
+            <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
+            <RatedDoctor />
+            <RatedDoctor />
+            <RatedDoctor />
+            <Text style={styles.sectionLabel}>Berita Terbaru</Text>
+            <NewsItem />
+            <NewsItem />
+            <NewsItem />
+            {/* Adding gap on the bottom */}
+            <Gap height={30} />
+          </View>
+        </ScrollView>
       </View>
     </View>
   );
@@ -49,8 +60,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   content: {
-    paddingVertical: 30,
-    paddingHorizontal: 16,
     flex: 1,
     backgroundColor: colors.white,
     // Create a border radius on the bottom of the page
@@ -77,5 +86,8 @@ const styles = StyleSheet.create({
     color: colors.text.primary,
     marginTop: 30,
     marginBottom: 16,
+  },
+  wrapperSection: {
+    paddingHorizontal: 16,
   },
 });
